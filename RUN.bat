ca8a313c4e3556b0acbd64f9df96fdb7e475d097
1976haru <99513847+1976haru@users.noreply.github.com>
2026-09-10 06:26:53 +0900

    make v3.7.2 the default runtime

--- RUN.bat
+++ RUN.bat
@@ -15,7 +15,10 @@ if not exist ".venv\Scripts\pythonw.exe" (
     exit /b 1
 )
 
-if exist "%~dp0Suno15_Mastering_v3_7_1.pyw" (
+if exist "%~dp0Suno15_Mastering_v3_7_2.pyw" (
+    start "" "%~dp0.venv\Scripts\pythonw.exe" "%~dp0Suno15_Mastering_v3_7_2.pyw"
+) else if exist "%~dp0Suno15_Mastering_v3_7_1.pyw" (
+    echo v3.7.2 adapter not found. Starting v3.7.1 application.
     start "" "%~dp0.venv\Scripts\pythonw.exe" "%~dp0Suno15_Mastering_v3_7_1.pyw"
 ) else if exist "%~dp0Suno15_Mastering_v3_7.pyw" (
     echo v3.7.1 adapter not found. Starting v3.7 application.
